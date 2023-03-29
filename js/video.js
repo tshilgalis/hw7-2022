b3bda0e83669dc624eb7b14988ac883a0c17715b
tshilgalis <56262446+tshilgalis@users.noreply.github.com>
2023-03-29 17:37:00 -0400

    Initial commit

---
+++ js/video.js
@@ -0,0 +1,62 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = "100";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log("Speed is: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate *= 1.1;
+	console.log("Speed is: " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if ((video.currentTime + 10) >= video.duration) {
+		video.currentTime = 0;
+		video.autoplay = false;
+	}
+	else {
+		video.currentTime += 10;
+	}
+	console.log("Video current time is " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {
+		video.muted = true;
+		this.innerHTML = "Unmute"
+	}
+	else {
+		video.muted = false;
+		this.innerHTML = "Mute"
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML = (video.volume * 100) + '%';
+});
+
+document.querySelector("#vintage").addEventListener("click", function () {
+	video.classList.add("oldSchool");
+})
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove('oldSchool');
+})
