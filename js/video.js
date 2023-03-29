9616bb74e6e653b6d5ad0ab4fed9a0908e4288da
tshilgalis <56262446+tshilgalis@users.noreply.github.com>
2023-03-29 17:50:37 -0400

    fixed play volume

--- js/video.js
+++ js/video.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	document.querySelector("#volume").innerHTML = "100";
+	document.querySelector("#volume").innerHTML = (video.volume * 100) + '%';
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
